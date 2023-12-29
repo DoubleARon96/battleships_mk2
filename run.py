33c9a95d4953ca8c377597dd453deef09754a946
DoubleARon96 <>
2023-12-29 09:10:20 +0000

    player input and imports added

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+import time
+import random
+
+# computer and player scores set to 0 
+scores = {"computer":0, "player":0}
+# Global variables for grid
+player_grid_rows = 5
+player_grid_column = 10
+cpu_grid_rows = 5
+cpu_grid_column = 10
+#global name variable 
+name = ()
+
+
+class Player_Input:
+    def __init__(self, player_name=None,):
+        if not player_name:
+            player_name = self.get_player_name()
+
+        self.player_name = player_name
